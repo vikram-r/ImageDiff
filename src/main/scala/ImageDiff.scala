f0cdfb3b28a7717d2d27084d2c0de7326c9dffba
Vikram Ramesh <vikram132@gmail.com>
2016-01-31 18:42:27 -0800

    Images are partitioned, sent to akka actors, then diffed
    The partition algorithm is temporarily basic (only uses the
    smallest width and height of the smaller image) to define the
    bounds of the image to diff

--- src/main/scala/ImageDiff.scala
+++ src/main/scala/ImageDiff.scala
@@ -1,7 +1,12 @@
+import java.awt.Rectangle
 import java.awt.image.BufferedImage
 import java.io.{IOException, File}
 import java.net.URI
 import javax.imageio.ImageIO
+
+import ImageProcessingActor._
+
+import akka.actor.{Props, ActorSystem}
 
 import scala.util.{Failure, Success, Try}
 
@@ -16,9 +21,21 @@ object ImageDiff {
 
     val initialImage = loadBufferedImage(args(0)).get
     val modifiedImage = loadBufferedImage(args(1)).get
+    val context = ActorSystem("ImageDiffActors")
 
-    println(s"${initialImage.getWidth}")
+    val COMP_SQUARE_SIZE = math.min(math.min(initialImage.getWidth, modifiedImage.getWidth),
+                                    math.min(initialImage.getHeight(), modifiedImage.getHeight))
+    val SUBIMG_DIM = 50
 
+    //todo this loop is currently dumb, it only iterates to the smallest width or height (instead of both)
+    for (i <- 0 until COMP_SQUARE_SIZE by SUBIMG_DIM){
+      val size = if (i + (SUBIMG_DIM) > COMP_SQUARE_SIZE) COMP_SQUARE_SIZE - i else SUBIMG_DIM
+
+      context.actorOf(Props(new ImageProcessingActor())) !
+        new SubImageMessage(initialImage.getSubimage(i, i, size, size),
+                            modifiedImage.getSubimage(i, i, size, size))
+
+    }
   }
 
   def loadBufferedImage(name: String) = Try(ImageIO.read(new File(s"./${name}"))) match {
@@ -32,6 +49,4 @@ object ImageDiff {
     ex.foreach(println)
     System.exit(1)
   }
-
-
 }
---
+++ src/main/scala/ImageProcessingActor.scala
@@ -0,0 +1,52 @@
+import java.awt.Color
+import java.awt.image.{ColorModel, BufferedImage, Raster}
+import java.io.File
+import javax.imageio.ImageIO
+
+import akka.actor.{ActorLogging, Actor}
+import akka.actor.Actor.Receive
+import akka.event.Logging
+
+/**
+  * Created by vikram on 1/28/16.
+  */
+
+object ImageProcessingActor {
+  val white = new Color(255, 255, 255)
+
+  case class Coord(x: Int,
+                   y: Int)
+
+  case class SubImageMessage(original: BufferedImage,
+                             modified: BufferedImage)
+}
+
+class ImageProcessingActor extends Actor with ActorLogging {
+  import ImageProcessingActor._
+
+  def receive = {
+    case SubImageMessage(original, modified) => {
+      log.info("Received subimage {}", original)
+
+    }
+    case _ => log.error("Could not understand message")
+  }
+
+  def diffImages(b1: BufferedImage, b2: BufferedImage): BufferedImage = {
+    //assume that both images are the same size
+    val coords = for (x <- 0 until b1.getWidth;
+                      y <- 0 until b1.getHeight) yield Coord(x, y)
+
+    val diff = new BufferedImage(b1.getWidth, b1.getHeight, BufferedImage.TYPE_INT_RGB)
+
+    coords.foldLeft(diff)((accum, coord) => {
+      log.info("{},{}", coord.x, coord.y)
+      accum.setRGB(coord.x, coord.y, diffPixel(b1.getRGB(coord.x, coord.y), b2.getRGB(coord.x, coord.y)))
+      accum
+    })
+  }
+
+  //if the pixels are different, return p2, otherwise return white
+  def diffPixel(p1: Int, p2: Int): Int = if (p1 == p2) white.getRGB else p2
+
+}
